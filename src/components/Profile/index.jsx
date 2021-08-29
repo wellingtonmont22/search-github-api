88dfc99b972c5223a25b3485b33b83b316b47a24
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-08-29 19:10:38 -0300

    adding githubprovider on the profile component

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -16,22 +16,26 @@ const Profile = () => {
         <div>
           <h1>{githubState.user.name}</h1>
           <S.WrapperUsername>
-            <h3>Username:</h3>
-            <a href="https://github.com/wellingtonmont22" target="_blank" rel="noreferrer" >wellingtonmont22</a>
+            <h3>{githubState.user.login}</h3>
+            <a href={githubState.user.html_url} target="_blank" rel="noreferrer" >wellingtonmont22</a>
           </S.WrapperUsername>
         </div>
         <S.WrapperStatusCount>
           <div>
             <h4>Followers</h4>
-            <span>5</span>
-          </div>
-          <div>
-            <h4>Starreds</h4>
-            <span>5</span>
+            <span>{githubState.user.followers}</span>
           </div>
           <div>
             <h4>Followings</h4>
-            <span>5</span>
+            <span>{githubState.user.following}</span>
+          </div>
+          <div>
+            <h4>Gists</h4>
+            <span>{githubState.user.public_gists}</span>
+          </div>
+          <div>
+            <h4>Repos</h4>
+            <span>{githubState.user.public_repos}</span>
           </div>
         </S.WrapperStatusCount>
       </S.WrapperInfoUser>
--- src/providers/github-provider.js
+++ src/providers/github-provider.js
@@ -7,21 +7,21 @@ export const GitHubContext = createContext({
 });
 
 const GithubProvider = ({ children }) => {
-    const [ githubState, setGithubState ] = useState({
-        user: {
-            login: undefined,
-            name: "undefined",
-            publicUrl: undefined,
-            blog: undefined,
-            location: undefined,
-            company: undefined,
-            followers: 0,
-            following: 0,
-            public_gists: 0,
-            public_repos: 0,
-        },
-        repositories: [],
-        starred: [],
+    const [githubState, setGithubState] = useState({
+      user: {
+        login: undefined,
+        name: undefined,
+        html_url: undefined,
+        blog: undefined,
+        location: undefined,
+        company: undefined,
+        followers: 0,
+        following: 0,
+        public_gists: 0,
+        public_repos: 0,
+      },
+      repositories: [],
+      starred: [],
     });
 
     const contextValue = {
