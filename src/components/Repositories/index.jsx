f4d24cc5810d8aee7792514685bd4343f906453d
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-08-29 16:05:24 -0300

    creating RepositoryItem component

--- src/components/Repositories/index.jsx
+++ src/components/Repositories/index.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import * as S from './styled';
+import * as S from "./styled";
+import RepositoryItem from "../RepositoryItem";
 
 const Repositories = () => {
   return (
-      <S.WrapperTabs
-        selectedTabClassName="is-selected"
-        selectedTabPanelClassName="is-selected">
-            <S.WrapperTabsList>
-                <S.WrapperTab>
-                    Repositories
-                </S.WrapperTab>
-                <S.WrapperTab>
-                    Starred
-                </S.WrapperTab>
-            </S.WrapperTabsList>
-            <S.WrapperTabPanel>
-                panel Repositories
-            </S.WrapperTabPanel>
-            <S.WrapperTabPanel>
-                panel Starred
-            </S.WrapperTabPanel>
-      </S.WrapperTabs>
+    <S.WrapperTabs
+      selectedTabClassName="is-selected"
+      selectedTabPanelClassName="is-selected"
+    >
+      <S.WrapperTabsList>
+        <S.WrapperTab>Repositories</S.WrapperTab>
+        <S.WrapperTab>Starred</S.WrapperTab>
+      </S.WrapperTabsList>
+      <S.WrapperTabPanel>
+        <RepositoryItem
+          name="repo 1"
+          linkToRepo="https://github.com/wellingtonmont22/aula_bootstrap"
+          fullName="Full name"
+        />
+      </S.WrapperTabPanel>
+      <S.WrapperTabPanel>
+        <RepositoryItem
+          name="repo 2"
+          linkToRepo="https://github.com/wellingtonmont22/aula_bootstrap"
+          fullName="Full name"
+        />
+      </S.WrapperTabPanel>
+    </S.WrapperTabs>
   );
 };
 
