56289896f2378cd5a240ce786a3463712d795a28
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-09-02 20:33:13 -0300

    creating header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,29 @@
-import React from "react";
-import * as S from './styled';
+import React, { useState } from "react";
+import * as S from "./styled";
+import useGithub from "../../hooks/github-hooks";
 
 const Header = () => {
+  const [usernameForSearch, setUsernameForSearch] = useState();
+
+  const { getUser } = useGithub();
+
+  const submitGetUser = () => {
+    if (!usernameForSearch) return;
+
+    return getUser(usernameForSearch);
+  };
+
   return (
     <header>
       <S.Wrapper>
-        <input type="text" placeholder="Digite o username para pesquisa..." />
-        <button><span>Buscar</span></button>
+        <input
+          type="text"
+          placeholder="Digite o username para pesquisa..."
+          onChange={(e) => setUsernameForSearch(e.target.value)}
+        />
+        <button type="submit" onClick={submitGetUser}>
+          <span>Buscar</span>
+        </button>
       </S.Wrapper>
     </header>
   );
